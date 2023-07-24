c9663527e6d241111ed140bd8b4b57c58ab8c02f
xdHampus <16954508+xdHampus@users.noreply.github.com>
2023-07-24 19:06:35 +0200

    Use stable pkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "prog";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     utils.url = "github:numtide/flake-utils";
     utils.inputs.nixpkgs.follows = "nixpkgs";
   };
